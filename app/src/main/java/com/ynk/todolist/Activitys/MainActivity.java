2a6decac4eea3189ec92c28ead2506bf5e3ef110
sarry94118 <liao.yi@northeastern.edu>
2021-11-12 14:45:25 -0800

    first create android app

---
+++ app/src/main/java/com/ynk/todolist/Activitys/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ynk.todolist.Activitys;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ynk.todolist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
