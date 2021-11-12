496c8c394ed55ed29a945caf8e2a43623b293396
sarry94118 <liao.yi@northeastern.edu>
2021-11-12 14:52:08 -0800

    add homepage and logo with background

---
+++ app/src/main/java/com/ynk/todolist/Activitys/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.ynk.todolist.Activitys;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ynk.todolist.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
